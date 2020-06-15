7ae9e6e590ed0c77a0939a1d31415de8ee759b39
brightLian <brightLian@126.com>
2020-06-15 20:09:24 +0800

    feat: 原始数据类型

--- basicsTS/.vuepress/config.js
+++ basicsTS/.vuepress/config.js
@@ -26,6 +26,17 @@ module.exports = {
                     '/chapterFirst/third.md',
                 ]
             },
+            {
+                title: '基础',   // 标题：必要的
+                path: '/chapterSecond/first',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                collapsable: true, // 展开侧边栏： 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 侧边栏深度：可选的, 默认值是 1
+                children: [
+                    '/chapterSecond/first.md',
+                    // '/chapterSecond/second.md',
+                    // '/chapterSecond/third.md',
+                ]
+            }
         ]
     }
 };
